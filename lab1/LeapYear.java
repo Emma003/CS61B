c52f76cc23bc0821791b1e53a85448e285fe1469
Emma <3lddemmanounke@gmail.com>
2021-11-18 18:52:46 -0500

    Completed lab 1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,15 @@ public class LeapYear {
         }
     }
 
+    /** Takes a year as an argument and returns true if it's a leap year and false otherwise */
+    public static boolean isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
